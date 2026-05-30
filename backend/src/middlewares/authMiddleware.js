f2fee95c5d20e61255a60613fbd7cb6ca4b44145
Anggirahmadillah <139853701+Anggirahmadillah@users.noreply.github.com>
2026-05-31 03:05:05 +0700

    fix multi user allocation and report

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,21 +1,58 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
 
 const protect = async (req, res, next) => {
+
   try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ message: "Token tidak ditemukan" });
+
+    const authHeader =
+      req.headers.authorization;
+
+    if (
+      !authHeader ||
+      !authHeader.startsWith("Bearer ")
+    ) {
+
+      return res.status(401).json({
+        message: "Token tidak ditemukan"
+      });
+
     }
-    const token = authHeader.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id).select("-password");
-    if (!user) return res.status(401).json({ message: "User tidak ditemukan" });
+
+    const token =
+      authHeader.split(" ")[1];
+
+    const decoded =
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET
+      );
+
+    const user =
+      await User.findById(
+        decoded.id
+      ).select("-password");
+
+    if (!user) {
+
+      return res.status(401).json({
+        message: "User tidak ditemukan"
+      });
+
+    }
+
     req.user = user;
+
     next();
+
   } catch {
-    res.status(401).json({ message: "Token tidak valid / expired" });
+
+    res.status(401).json({
+      message: "Token tidak valid / expired"
+    });
+
   }
+
 };
 
-module.exports = protect;
+export default protect;
